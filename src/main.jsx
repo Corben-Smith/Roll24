e1bde532b1a632fa58fb5c7c554139047bc048c2
Corben-Smith <corbensmiht782@gmail.com>
2024-04-10 23:22:24 -0400

    Made Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import App from './App.jsx'
 import './site.css'
+import Board from './Board.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Map" element={<App />} />
+          <Route path="/Map" element={<App />} />
+          <Route path="*" element={<p>Not Found Sucker</p>} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
