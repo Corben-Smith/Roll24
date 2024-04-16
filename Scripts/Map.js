52375c589f580c337a1ec562e4dbe5ac05178380
Corben-Smith <corbensmiht782@gmail.com>
2024-04-16 11:46:00 -0400

    Created Map Class

--- Scripts/Map.js
+++ Scripts/Map.js
@@ -1,5 +1,18 @@
+import Token from './Token'
 export default class Map {
-    constructor(title) {
-        this.title = title
+    constructor(image, tokens, cellDimensions) {
+        if (typeof image !== 'string' || !Array.isArray(tokens) || tokens.some(token => !(token instanceof Token))) {
+            throw new Error('Invalid argument types');
+        }
+        
+        this.image = image,
+        this.tokens = tokens
+        this.cellDimensions = cellDimensions
+
+        const img = new Image()
+        img.onload = () => {
+            this.mapDimensions = { width: img.width + 'px', height: img.height + 'px' }
+        }
+        img.src = image;
     }
 }
