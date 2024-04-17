a4f523da6a24fd4c873b76c5416d68624ee527ab
Corben-Smith <corbensmiht782@gmail.com>
2024-04-16 23:18:30 -0400

    UGH

--- Scripts/Token.js
+++ Scripts/Token.js
@@ -1,6 +1,6 @@
 import Status from './Status'
 export default class Token {
-    constructor(name, image, status) {
+    constructor(id, name, image, status) {
         if (typeof image !== 'string' || typeof name !== 'string' || !(status instanceof Status)) {
             throw new Error('Invalid argument types');
         }
