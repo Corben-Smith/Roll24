66a1203f87552433e9021033b1cd794f4dbc727b
Riley Hayden <Haydenr@etsu.edu>
2024-04-24 09:22:09 -0400

    tada

--- src/MapCreator.jsx
+++ src/MapCreator.jsx
@@ -2,6 +2,9 @@ import React, {useRef, useState} from 'react';
 
 import './site.css'
 import { PhotoInput } from './PhotoInput';
+import Map from '../Scripts/Map';
+
+
 
 
 
@@ -9,36 +12,56 @@ import { PhotoInput } from './PhotoInput';
 export default function UI(props) {
   const [columns, setColumns] = useState(10);
   const [rows, setRows] = useState(10);
-  const [image, setSelectedImage] = useState(localStorage.getItem("savedImage"))
+  const [cellHeight, setCellHeight] = useState()
+  const [cellWidth, setCellWidth] = useState();
+  const [image, setSelectedImage] = useState(localStorage.getItem("savedMapImage"))
+  const [tokens, setTokens] = useState(localStorage.getItem("myTokens"))
 
   function handleSubmit(event){
-    event.preventDefault()
+    var map = new Map(image, tokens, [cellWidth, cellHeight]);
+    localStorage.setItem("savedMap", map);
   }
 
   function handleChangeColumns(event){
     setColumns(event.target.value)
+    setCellWidth(1/event.target.value)
   }
   function handleChangeRows(event){
     setRows(event.target.value)
+    setCellHeight(1/event.target.value)
   }
-
-
+    
+  
   
   return(
-      <div>      
-      <section>
+      <div className='ml-'>      
+      <section className='flex flex-left max-w-4xl p-1 float-center ml-6'>
         <form onSubmit={handleSubmit}>        
-        <label>Upload Map Image:</label>
-        <PhotoInput value = {image} onChange = {setSelectedImage}/>
-          <label>
+        <text className=' text-white border-dashed border-2 px-2 py-0 border-white'><b>Map Creator</b></text>
+          <br/>
+          <label className="text-white">
             Columns:
-            <input type="number" value={columns} onChange={handleChangeColumns}/>        
+            <input type="number" className='block w-full mx-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring' value={columns} onChange={handleChangeColumns}/>        
           </label>
-          <label>
+          <label className="text-white ">
             Rows:
-            <input type="text" value={rows} onChange={handleChangeRows}/>        
+            <input type="text" className='block w-full mx-4  py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring' value={rows} onChange={handleChangeRows}/>        
           </label>
-          <input type="submit" value="Submit" />
+          
+          <div>
+                    <label className="block text-sm font-medium text-white">Map Image</label>
+                  <div className="mt-1 flex justify-center mx-4 px-6 pt-5 pb-6 border-2 border-white border-dashed rounded-md">
+                    <div className="space-y-1 text-center">
+                      <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                          <PhotoInput onChange ={setSelectedImage}/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <input type="submit" className='px-3 mr-10 py-1 leading-5 text-#000 transition-colors duration-200 bg-light-green rounded-md hover:bg-dark-green hover:text-white focus:outline-none focus:bg-gray-600 mt-2 ml-2' value="Submit" onClick={handleSubmit}/>
         </form>
         </section>
       </div>
--- src/TokenCreatorUI.jsx
+++ src/TokenCreatorUI.jsx
@@ -8,12 +8,13 @@ import Status from '../Scripts/Status';
 import { PhotoInput } from './PhotoInput';
 import { renderIntoDocument } from 'react-dom/test-utils';
 import ConditionSelect from './ConditionSelect';
+import Border from './Board.jsx';
 
 
 
 export default function TokenCreatorUI(props) {
   const [name, setName] = useState("")
-  const [image, setSelectedImage] = useState(localStorage.getItem("savedImage"))
+  const [image, setSelectedImage] = useState(localStorage.getItem("savedToken"))
   const [statusName, setStatusName] = useState()
   const [statusColor, setStatusColor] = useState()
   const [status, setStatus] = useState()
@@ -35,7 +36,8 @@ export default function TokenCreatorUI(props) {
   function TokenCreator(){
 
      const token = new Token(name, image, status, armourClass, healthPoints, initiative)
-     setPlayerToken(token)
+     playerToken.push(token)
+     
   }
   function handleChangeName(event){
     setName(event.target.value)
@@ -56,20 +58,20 @@ export default function TokenCreatorUI(props) {
     <div className='bg-blue'> 
       <section className="flex flex-left max-w-4xl p-1 float-right">
         <form>
-            <div className=" gap-1 mt-4 sm:grid-cols-2 float-left mr-6">
+            <div className=" gap-1 mt-4 sm:grid-cols-2 float-right mr-6">
               <text className=' text-white border-dashed border-2 px-2 py-0 border-white'><b>Token Creator</b></text>
                 <div>
-                    <label className="text-white dark:text-gray-200" value="name">Character Name</label>
+                    <label className="text-white " value="name">Character Name</label>
                     <input id="name" type="string" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={handleChangeName}/>
                 </div>
 
                 <div>
-                    <label className="text-white dark:text-gray-200" value="AC">Armour Class</label>
+                    <label className="text-white dark:text-gray-200" value="armourclass">Armour Class</label>
                     <input id="armourClass" type="number" min='10' max='50' className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={handleChangeAC}/>
                 </div>
 
                 <div>
-                    <label className="text-white dark:text-gray-200" value="HP">Health Points</label>
+                    <label className="text-white dark:text-gray-200" value="healthpoints">Health Points</label>
                     <input id="hitPoints" type="number" min='0'  max='600' className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" onChange={handleChangeHP}/>
                 </div>
                 <div>
@@ -80,7 +82,7 @@ export default function TokenCreatorUI(props) {
                   <ConditionSelect  onChange={handleChangeStatusName}/>
                 </div>
                 <div>
-                    <label className="block text-sm font-medium text-white">Image</label>
+                    <label className="block text-sm font-medium text-white">Token Image</label>
                   <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-white border-dashed rounded-md">
                     <div className="space-y-1 text-center">
                       <svg className="mx-auto h-12 w-12 text-white" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -93,7 +95,8 @@ export default function TokenCreatorUI(props) {
                   </div>
                 </div>
                 <div className="flex flex-left">
-                <button className="px-3 py-1 leading-5 text-#000 transition-colors duration-200 bg-light-green rounded-md hover:bg-dark-green hover:text-white focus:outline-none focus:bg-gray-600 mt-2" onClick={StatusCreator && TokenCreator}>Save</button>
+                <button className="px-3 py-1 leading-5 text-#000 transition-colors duration-200 bg-light-green rounded-md hover:bg-dark-green hover:text-white focus:outline-none focus:bg-gray-600 mt-2" onClick={StatusCreator && TokenCreator}>Create</button>
+                <button className="px-3 py-1 leading-5 text-#000 transition-colors duration-200 bg-light-green rounded-md hover:bg-dark-green hover:text-white focus:outline-none focus:bg-gray-600 mt-2 ml-2" onClick={localStorage.setItem('myTokens', JSON.stringify(playerToken))}>Save</button>
             </div>
             </div>
         </form>
